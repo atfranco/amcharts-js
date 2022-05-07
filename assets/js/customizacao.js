5c1e58d5e93efcc7ba11ccc2e90fb43f57da5fb3
tutao <artfco@yahoo.com.br>
2022-05-07 15:43:46 -0300

    update

--- assets/js/customizacao.js
+++ assets/js/customizacao.js
@@ -7,29 +7,28 @@ var chart = am4core.create("grafico-balls", am4plugins_forceDirected.ForceDirect
 var networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
 
 chart.data = [
+
   {
-    name: "Linguagens",
+    name: "Habilidades",
     children: [
       {
         name: "FRONTEND",
         children: [
           {
-            name: "CSS",
-            children: [
-              { name: "SCSS", value: 90 },
-              { name: "BOOTSTRAP", value: 87 },
-              { name: "TAILWIND.CSS", value: 55 }
+            name: "CSS", value: 30, children: [
+              { name: "Scss/Sass", value: 8,},
+              { name: "Bootstrap", value: 8 },
+              { name: "Tailwind", value: 5 }
             ]
           },
-          { name: "HTML", value: 148 },
+          { name: "HTML5", value: 30 },
           {
-            name: "JAVASCRIPT", children: [
-                { name: "JQUERY", value: 70 },
-                { name: "REACT", value: 50 },
-                { name: "THREE.JS", value: 30 }
+            name: "JAVASCRIPT", value: 30, children: [
+                { name: "Jquery", value: 8 },
+                { name: "React", value: 5 },
+                { name: "Three.Js", value: 2 }
             ]
-          },
-          { name: "C4", value: 26 }
+          }
         ]
       },
       {
@@ -38,17 +37,52 @@ chart.data = [
           {
             name: "E1",
             children: [
-              { name: "EE1", value: 33 },
-              { name: "EE2", value: 40 },
-              { name: "EE3", value: 89 }
+              { name: "EE1", value: 1 },
+              { name: "EE2", value: 1 },
+              { name: "EE3", value: 1 }
             ]
           },
           {
             name: "E2",
-            value: 48
+            value: 1
+          }
+        ]
+      },
+      {
+        name: "DESIGN",
+        children: [
+          {
+            name: "E1",
+            children: [
+              { name: "EE1", value: 1 },
+              { name: "EE2", value: 1 },
+              { name: "EE3", value: 1 }
+            ]
+          },
+          {
+            name: "E2",
+            value: 1
+          }
+        ]
+      },
+      {
+        name: "SOFTWARES", value: 30,
+        children: [
+          {
+            name: "E1",
+            children: [
+              { name: "EE1", value: 1 },
+              { name: "EE2", value: 1 },
+              { name: "EE3", value: 1 }
+            ]
+          },
+          {
+            name: "E2",
+            value: 1
           }
         ]
       }
+
 
     ]
   }
@@ -58,15 +92,13 @@ networkSeries.dataFields.value = "value";
 networkSeries.dataFields.name = "name";
 // add id field
 networkSeries.dataFields.id = "name";
-
 networkSeries.dataFields.children = "children";
-networkSeries.nodes.template.tooltipText = "{name}:{value}";
+networkSeries.nodes.template.tooltipText = "{name}";
 networkSeries.nodes.template.fillOpacity = 1;
 networkSeries.manyBodyStrength = -20;
 networkSeries.links.template.strength = 0.8;
 networkSeries.minRadius = am4core.percent(2);
-
 networkSeries.nodes.template.label.text = "{name}"
-networkSeries.fontSize = 10;
+networkSeries.fontSize = 20;
 
 
